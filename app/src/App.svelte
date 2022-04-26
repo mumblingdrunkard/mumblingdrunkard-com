1e9424478a1f614effdc1e0caec65359edbf2f48
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-04-26 07:59:37 +0200

    add link to GH

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -5,6 +5,13 @@
 
 <main>
   <Router {routes} options={{ prefix: "app" }} />
+
+  <p>
+    See the source code for this on
+    <a href="https://github.com/mumblingdrunkard/mumblingdrunkard-com">
+      GitHub
+    </a>
+  </p>
 </main>
 
 <style>
